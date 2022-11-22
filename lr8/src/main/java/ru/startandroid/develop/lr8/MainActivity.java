4ca8b7149c00b7426499711237db3b3f580117ec
Fay <Fay.Spaniel@yandex.ru>
2022-11-22 11:51:19 +0300

    Initial commit

---
+++ lr8/src/main/java/ru/startandroid/develop/lr8/MainActivity.java
@@ -0,0 +1,17 @@
+package ru.startandroid.develop.lr8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onButton2Click(){
+        System.exit(0);
+    }
+}
